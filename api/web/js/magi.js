0c879b4cf85df0345597b2b92137392a1d76cb09
fpem <pengfj@gmail.com>
2014-11-10 17:36:21 +0800

    Update magi.js

--- api/web/js/magi.js
+++ api/web/js/magi.js
@@ -3,3 +3,16 @@ var MagiMsg={
     DataTableNoFound: 'DataTable {1} No Found',
     DataSchemaNoFound: 'Fatal: No data schema found for Magi-App {1}'
 }
+
+
+var DataResult = function(rows, totalRow){
+    this.rows = rows;
+    this.totalRow = totalRow;
+}
+var QueryCriteria = function(pageNum, pageSize, criteria, sortBy, groupBy){
+    this.pageNum = pageNum ? pageNum : 1;
+    this.pageSize = pageSize ? pageSize : 1;
+    this.criteria = criteria;
+    this.sortBy = sortBy;
+    this.groupBy = groupBy;
+}
