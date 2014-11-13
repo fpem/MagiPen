5716bfc47f6d227521bdfd37f6be3cfd16cc7a08
fpem <pengfj@gmail.com>
2014-11-13 17:44:28 +0800

    Update magiDao.js

--- api/web/js/magiDao.js
+++ api/web/js/magiDao.js
@@ -1,38 +1,40 @@
-var MagiDao = (function(){
-    //var MagiDefaultApp ;
-    var MagiDataModels = {}; //{app : {tables: {}, i18n:{}}
-    var MagiDataCache= {}; //todo create web socket to refresh data
-    //var MagiEndPoints= {};
-    checkEnv();
-    initData();
+var MagiDao = {	
+    magiDataModels: {}, //{app : {tables: {}, i18n:{}}
+    magiDataCache: {}, //todo create web socket to refresh data
+    
+	//to do check environment
+	checkEnv: function(){
+		if( typeof MagiSetting == 'undefined' || !MagiSetting.app || !MagiSetting.endPoint ){
+			console.log('ERROR: magiConfig.js is not found or incorrect!');
+			alert('ERROR: magiConfig.js is not found or incorrect!');//todo i18n
+		}
+	},		
 
-    function MagiAjax(url, _settings){
+    magiAjax: function(url, _settings){
         var settings= $.extend({
                 cache:false,
                 dataType: 'json',
-                contentType: 'application/json',
+                //contentType: 'application/json',
                 type: 'GET'
             },
             _settings
         );
-        var succHandle= settings.success;
         settings.success= function(resp){
-            succHandle(resp);
+			if(_settings.success) _settings.success(resp);
             //todo refresh cache for POST/PUT/DEL
-        }
-        var errHandle= settings.error;
+        };
         settings.error= function(jqXHR, textStatus, errorThrown){
-            errHandle(jqXHR, textStatus, errorThrown);
+            if(_settings.error) _settings.error(jqXHR, textStatus, errorThrown);
             //todo common error handle
-        }
+        };
         $.ajax(url, settings);
-    }
+    },
 
-    function MagiDaoException(jqXHR, textStatus, errorThrown){
+    magiDaoException: function(jqXHR, textStatus, errorThrown){
         alert('Failed to Connect the backend services.')
-    }
+    },
 
-    function getMagiMsg(errType, ref){
+    getMagiMsg: function(errType, ref){
         if(!errType)
             return MagiMsg['UnknownError'];
         var msg= MagiMsg[errType];
@@ -44,109 +46,137 @@ var MagiDao = (function(){
                 msg= msg.replace(re, ref[i]);
             }
         return msg;
-    }
+    },
 
-    function checkEnv(){
-        //todo check browser
-        if( typeof MagiSetting == 'undefined' || !MagiSetting.appName || !MagiSetting.endPoint ){
-            console.log('ERROR: magiConfig.js is not found or incorrect!');
-            alert('ERROR: magiConfig.js is not found or incorrect!');//todo i18n
-        }
-    }
-    //attach app, load app data moder, and ref data to MagiCache
-    function initData(){
-        //to do retrieve data schema by web service
-        var dataModels= $.ajax({url:MagiSetting.endPoint+ 'schema',async:false, error: MagiDaoException});
+	_app: function(appName){ return appName? appName : MagiSetting.app;},
 
-        if(dataModels.length==0) {
-            alert(getMagiMsg("DataSchemaNoFound", app));
-            return;
-        }
+	defaultCriteria: {pageNum: 1, pageSize: 10},
+		
+		//load app data model, and cache data for cacheable tables
+    initData: function(appName){
+		var app= MagiDao._app(appName);
+		MagiDao.checkEnv();
+		//to do retrieve data schema by web service
+		var urlDataModels= MagiSetting.endPoint + 'dataModel/'+ app;
+		//var dataModels= $.get({url: urlDataModels,async:false, error: MagiDao.magiDaoException});
+		MagiDao.magiAjax(urlDataModels, {async: false, error: function() { 
+				alert(MagiDao.getMagiMsg("DataModelNoFound", app));
+			},
+			success: function(dataModels) {
+				Log4Js.debug('loaded data models for app : '+ app+ ', dataModel:\n'+ dataModels);
+				if(!dataModels) {
+					alert(MagiDao.getMagiMsg("DataModelNoFound", app));
+					return;
+				}
+				MagiDao.magiDataModels[app]= dataModels;
+				
+				//to do load cache        
+				MagiDao.magiDataCache[app]= {};
+				$.each( dataModels.tables, function(tableName, tableModel){
+					if(tableModel.cacheable){
+						MagiDao.find(tableName, null, function(dataResult){
+							MagiDao.magiDataCache[app][tableName]= 
+								(dataResult && dataResult.rows)? dataResult.rows : [];
+						});
+					}
+				}); 
+			}
+		});
+	},
+	getEndPoint: function(appName){
+		//if(extModule)
+		//    return MagiSetting.extEndPoints[extModule];
+		//else
+			return MagiSetting.endPoint + 'data/'+ MagiDao._app(appName)+ '/';
+	},
+	getAppDataModels: function(appName){
+		return MagiDao.magiDataModels[MagiDao._app(appName)];
+	},
+	getDataModel: function(tableName, appName){
+		var appModel= MagiDao.getAppDataModels(appName);
+		if(appModel && appModel.tables && appModel.tables[tableName])
+			return appModel.tables[tableName];
+		return null;
+	},
+	isCached: function(tableName, appName){
+		var app= MagiDao._app(appName);
+		if(MagiDao.magiDataCache[app] && MagiDao.magiDataCache[app][tableName])
+			return true;
+	},
+	getCachedTable: function(tableName, appName){
+		var app= MagiDao._app(appName);
+		if(MagiDao.magiDataCache[app])
+			return MagiDao.magiDataCache[app][tableName];
+		return null;
+	},
+	getCachedEntry: function(tableName, fieldValue, fieldName, appName){
+		var app= MagiDao._app(appName);
+		var field= fieldName ? fieldName : 'id';
+		if(MagiDao.magiDataCache[app] && MagiDao.magiDataCache[app][tableName]){
+			for(var i=0; i++; i< MagiDao.magiDataCache[app][tableName].length){
+				var entry= MagiDao.magiDataCache[app][tableName][i];
+				if(entry[field] == fieldValue)
+					return entry;
+			}
+		}
+		return null;
+	},
+	findById: function(dataTable, id, succHandle, errHandle, app){
+		if(MagiDao.isCached(dataTable, app)){
+			succHandle( MagiDao.getCachedEntry(dataTable, id, app) );
+			return;
+		}
+		MagiDao.magiAjax(MagiDao.getEndPoint(app) + dataTable + '/' + id, {success: succHandle, error: errHandle});
+	},
+	find: function(dataTable, queryCriteria, gridElement, succHandle, errHandle, app){
+		if(MagiDao.isCached(dataTable, app)){
+			if(succHandle) succHandle( MagiDao.getCachedTable(dataTable, app) );
+			return;
+		}
+		MagiDao.magiAjax(MagiDao.getEndPoint(app) + dataTable, {data: queryCriteria, success: function(_dataResult){				
+				// debug ---------------------
+				var tableRows= _dataResult.rows;
+				var rows= [];
+				var qc = jQuery.extend({}, MagiDao.defaultCriteria, queryCriteria);
+				if(tableRows && qc.criteria){
+					var filteredRows= [];
+					for(var i= 0;  i< tableRows.length; i++){
+						if( CriteriaOpr[qc.criteria.opr] (tableRows[i][qc.criteria.left], qc.criteria.right) )
+							filteredRows.push(tableRows[i]);
+					}
+					tableRows= filteredRows;
+				}
+				var start= (qc.pageNum-1) * qc.pageSize;
+				for(var i= start; tableRows && i< tableRows.length && i< start+ qc.pageSize; i++){
+					rows.push(tableRows[i]);
+				}
+				var dataResult= new DataResult(rows, tableRows.length);
+				// debug ---------------------
+				
+				if(succHandle) succHandle(dataResult, gridElement);
+			}, error: errHandle});
+	},
+	
+	postData: function(dataTable, entry, succHandle, errHandle, app){
+		MagiDao.magiAjax(MagiDao.getEndPoint(app) + dataTable, {type: 'POST', data: entry, success: succHandle, error: errHandle});
+	},
+	putData: function(dataTable, id, entry, succHandle, errHandle, app){
+		MagiDao.magiAjax(MagiDao.getEndPoint(app) + dataTable+ '/' + id, {type: 'PUT', data: entry, success: succHandle, error: errHandle});
+	},
+	deleteData: function(dataTable, id, succHandle, errHandle, app){
+		MagiDao.magiAjax(MagiDao.getEndPoint(app) + dataTable+ '/' + id, {type: 'DELETE', success: succHandle, error: errHandle});
+	},
+	/**
+	 *
+	 * @param persistRequests  [{tableName:'xx', data: 'id or entry'}, opr: 'INSERT/UPDATE/DELETE'}]
+	 * @param succHandle
+	 * @param errHandle
+	 * @param app
+	 */
+	batchPersist: function(persistRequests, succHandle, errHandle, app){
+		MagiDao.magiAjax(MagiDao.getEndPoint(app) + 'batchPersist', {type: 'POST', data: persistRequests, success: succHandle, error: errHandle});
+	}
 
-        MagiDataModels[app]= dataModels;
-        //todo load cache
-        var cacheTables= [];
-        $.each( dataModels.tables, function(){
-            if(this.cacheable){
-                cacheTables.push(this.name);
-            }
-        });
-        MagiDataCache[app]= cacheTables;
-        console.log('Cached tables: '+ cacheTables.join());
-    }
+};
 
-    return {
-
-        getEndPoint: function(extModule){
-            if(extModule)
-                return MagiSetting.extEndPoints[extModule];
-            else
-                return MagiSetting.endPoint;
-        },
-        getDataModel: function(tableName, appName){
-            var app= appName? appName : MagiSetting.appName;
-            return MagiDataModels[app]
-        },
-        getDataTable: function(tableName, appName){
-            var app= appName? appName : MagiSetting.appName;
-            return MagiDataModels[app][tableName];
-        },
-        isCached: function(tableName, appName){
-            var app= appName? appName : MagiSetting.appName;
-            if(MagiDataCache[app] && MagiDataCache[app][tableName])
-                return true;
-        },
-        getCachedTable: function(tableName, appName){
-            var app= appName? appName : MagiSetting.appName;
-            if(MagiDataCache[app])
-                return MagiDataCache[app][tableName];
-            return null;
-        },
-        getCachedEntry: function(tableName, fieldValue, fieldName, appName){
-            var app= appName? appName : MagiSetting.appName;
-            var field= fieldName ? fieldName : 'id';
-            if(MagiDataCache[app] && MagiDataCache[app][tableName]){
-                for(var i=0; i++; i< MagiDataCache[app][tableName].length){
-                    var entry= MagiDataCache[app][tableName][i];
-                    if(entry[field] == fieldValue)
-                        return entry;
-                }
-            }
-            return null;
-        },
-        findDataRow: function(dataTable, id, succHandle, errHandle, app){
-            if(MagiDao.isCached(dataTable, app)){
-                succHandle( MagiDao.getCachedEntry(dataTable, id, app) );
-                return;
-            }
-            MagiAjax(MagiDao.getEndPoint() + dataTable + '/' + id, {success: succHandle, error: errHandle});
-        },
-        findDataList: function(dataTable, options, succHandle, errHandle, app){
-            if(MagiDao.isCached(dataTable, app)){
-                succHandle( MagiDao.getCachedTable(dataTable, app) );
-                return;
-            }
-            MagiAjax(MagiDao.getEndPoint(app) + dataTable, {data: options, success: succHandle, error: errHandle});
-        },
-        postData: function(dataTable, entry, succHandle, errHandle, app){
-            MagiAjax(MagiDao.getEndPoint(app) + dataTable, {type: 'POST', data: entry, success: succHandle, error: errHandle});
-        },
-        putData: function(dataTable, id, entry, succHandle, errHandle, app){
-            MagiAjax(MagiDao.getEndPoint(app) + dataTable+ '/' + id, {type: 'PUT', data: entry, success: succHandle, error: errHandle});
-        },
-        deleteData: function(dataTable, id, succHandle, errHandle, app){
-            MagiAjax(MagiDao.getEndPoint(app) + dataTable+ '/' + id, {type: 'DELETE', success: succHandle, error: errHandle});
-        },
-        /**
-         *
-         * @param persistRequests  [{tableName:'xx', data: 'id or entry'}, opr: 'INSERT/UPDATE/DELETE'}]
-         * @param succHandle
-         * @param errHandle
-         * @param app
-         */
-        batchPersist: function(persistRequests, succHandle, errHandle, app){
-            MagiAjax(MagiDao.getEndPoint(app) + 'batchPersist', {type: 'POST', data: persistRequests, success: succHandle, error: errHandle});
-        }
-    };
-
-});
+MagiDao.initData();
